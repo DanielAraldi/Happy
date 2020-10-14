1d0f047af388c5c33c34ce2e3fa7a81aecffc449
Daniel <danielsaraldi@gmail.com>
2020-10-14 09:40:57 -0300

    Fazendo abstração de componentes.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import leaflet from 'leaflet';
 
-import 'leaflet/dist/leaflet.css'
 import '../styles/pages/orphanages-map.css'
 
 import mapMarkerImg from '../images/map-marker.svg';
-
-const mapIcon = leaflet.icon({
-    iconUrl: mapMarkerImg,
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2],
-})
+import mapIcon from '../utils/mapIcon';
 
 function OrphanagesMap() {
     return(
