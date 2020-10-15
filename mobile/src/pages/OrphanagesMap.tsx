04a2a12b23b1018bcfec9417e9ce6499a59ec09e
Daniel <danielsaraldi@gmail.com>
2020-10-15 13:20:25 -0300

    Fazendo rotas nas páginas e começando a OrphanageDetails Page.

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from "react-native";
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
+
+import mapMarker from "../images/map-marker.png";
+
+export default function OrphanagesMap() {
+    const navigation = useNavigation();
+
+    function handleNavigateOrphanageDetails() {
+        navigation.navigate('OrphanageDetails');
+    }
+
+    return (
+        <View style={styles.container}>
+        <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            initialRegion={{
+            latitude: -27.0567732,
+            longitude: -48.8699401,
+            latitudeDelta: 0.008,
+            longitudeDelta: 0.008,
+            }}
+        >
+            <Marker
+            icon={mapMarker}
+            calloutAnchor={{
+                x: 2.7,
+                y: 0.8,
+            }}
+            coordinate={{
+                latitude: -27.0567732,
+                longitude: -48.8699401,
+            }}
+            >
+            <Callout tooltip onPress={handleNavigateOrphanageDetails}>
+                <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}>Lar das meninas</Text>
+                </View>
+            </Callout>
+            </Marker>
+        </MapView>
+
+        <View style={styles.footer}>
+            <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+            <TouchableOpacity
+            style={styles.createOrphanageButton}
+            onPress={() => {}}
+            >
+            <Feather name="plus" size={28} color="#FFF" />
+            </TouchableOpacity>
+        </View>
+        </View>
+    );
+    }
+
+    const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
+
+    calloutContainer: {
+        width: 160,
+        height: 46,
+        paddingHorizontal: 16,
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        borderRadius: 16,
+        justifyContent: "center",
+    },
+
+    calloutText: {
+        color: "#0089a5",
+        fontSize: 14,
+        fontFamily: "Nunito_700Bold",
+    },
+
+    footer: {
+        position: "absolute",
+        left: 24,
+        right: 24,
+        bottom: 32,
+        backgroundColor: "#FFF",
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 24,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        elevation: 3, // Para sobrear apenas no ANDROID, do IOS temos o Shadow
+    },
+
+    footerText: {
+        color: "#8FA7B3",
+        fontFamily: "Nunito_700Bold",
+    },
+
+    createOrphanageButton: {
+        width: 56,
+        height: 56,
+        backgroundColor: "#15C3D6",
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
