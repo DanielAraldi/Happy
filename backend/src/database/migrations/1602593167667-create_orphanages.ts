b3e1606ca7653f1a2e34baadf9f967468c03edd3
Daniel <danielsaraldi@gmail.com>
2020-10-17 12:46:09 -0300

    Manutenções no banco de dados e inserção de favicon.
    Imagens antigas foram deletadas da pasta uploads, pasta .github criada e mais algumas alterações foram feitas.

--- backend/src/database/migrations/1602593167667-create_orphanages.ts
+++ backend/src/database/migrations/1602593167667-create_orphanages.ts
@@ -13,35 +13,35 @@ export class createOrphanages1602593167667 implements MigrationInterface {
           unsigned: true,
           isPrimary: true,
           isGenerated: true,
-          generationStrategy: 'increment'
+          generationStrategy: 'increment',
         },
         {
           name: 'name',
-          type: 'varchar'
+          type: 'varchar',
         },
         {
           name: 'latitude',
           type: 'decimal',
           scale: 10,
-          precision: 2
+          precision: 2,
         },
         {
           name: 'longitude',
           type: 'decimal',
           scale: 10,
-          precision: 2
+          precision: 2,
         },
         {
           name: 'about',
-          type: 'text'
+          type: 'text',
         },
         {
           name: 'instructions',
-          type: 'text'
+          type: 'text',
         },
         {
           name: 'opening_hours',
-          type: 'varchar'
+          type: 'varchar',
         },
         {
           name: 'open_on_weekends',
@@ -56,7 +56,7 @@ export class createOrphanages1602593167667 implements MigrationInterface {
 
   // Desfaz tudo feito no método UP, porém ao contrário
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('orphanages');
+    await queryRunner.dropTable('orphanages')
   }
 
 }
