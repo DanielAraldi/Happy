c5e0629bcda731872378465bedc57764bf738a55
Daniel <danielsaraldi@gmail.com>
2020-10-21 15:13:32 -0300

    Responsividade da página de orfanatos feita.
    Manutenções no banco de dados, exclusão de imagens da pasta uploads e mudança na url da imagem dos orfanatos também foram feitas.

--- backend/src/views/image_view.ts
+++ backend/src/views/image_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            url: `http://192.168.0.109:3333/uploads/${image.path}`,
+            url: `http://192.168.56.1:3333/uploads/${image.path}`, /* Ficar atento a mudanças da url */
         }
     },
 
